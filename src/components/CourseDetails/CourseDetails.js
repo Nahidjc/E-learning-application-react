56919b876e69b6fc1a793d306a6c451564b5893e
Nahidjc <nahidjc80@gmail.com>
2021-10-05 07:47:34 +0600

    course details

--- src/components/CourseDetails/CourseDetails.js
+++ src/components/CourseDetails/CourseDetails.js
@@ -1,5 +1,14 @@
 import { useParams } from "react-router-dom";
 import React, { useEffect, useState } from 'react';
+import ModalVideo from 'react-modal-video'
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import ButtonBase from '@mui/material/ButtonBase';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import PersonIcon from '@mui/icons-material/Person';
+import QuizIcon from '@mui/icons-material/Quiz';
 const CourseDetails = () => {
     const { id } = useParams();
 
@@ -28,13 +37,59 @@ const CourseDetails = () => {
     console.log(find);
 
 
+    const [isOpen, setOpen] = useState(false)
 
-
-
+    const Img = styled('img')({
+        margin: 'auto',
+        display: 'block',
+        maxWidth: '100%',
+        maxHeight: '100%',
+    });
     return (
-        <div>
-            {courses.length}
-            <h1>{find.courseName}</h1>
+        <div className="container p-5">
+            <Paper sx={{ p: 2, margin: 'auto', maxWidth: 'auto', flexGrow: 1 }}>
+                <Grid container spacing={2}>
+                    <Grid xs={4} item>
+                        <ButtonBase sx={{ height: 228 }}>
+                            <Img style={{ 'borderRadius': '10px' }} alt="complex" src={find.img} />
+                        </ButtonBase>
+                    </Grid>
+                    <Grid item xs={8} sm container>
+                        <Grid item xs container direction="column" spacing={2}>
+                            <Grid item xs>
+                                <Typography gutterBottom variant="subtitle1" component="div">
+                                    {find.courseName}
+                                </Typography>
+                                <Typography variant="body2" gutterBottom>
+                                    <div className="d-flex ">
+
+                                        <p className='me-4'><PlayArrowIcon></PlayArrowIcon> {find.Lesson} lessons </p>
+                                        <p className='me-4'><QuizIcon></QuizIcon> 10 quiz</p>
+                                        <p className='me-4'><PersonIcon></PersonIcon> 17 Students Enrolled</p>
+                                    </div>
+
+                                </Typography>
+                                <Typography variant="body2" gutterBottom>
+                                    {find.details}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    ID: 1030114
+                                </Typography>
+                            </Grid>
+                            <Grid item>
+                                <Typography sx={{ cursor: 'pointer' }} variant="body2">
+                                    Remove
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                        <Grid item>
+                            <Typography variant="subtitle1" component="div">
+                                ${find.price}
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Paper>
         </div>
     );
 };
