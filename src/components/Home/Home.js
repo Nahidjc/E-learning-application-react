f79c4b73244afc92defdcbc2b71e5b93ff7ee354
Nahidjc <nahidjc80@gmail.com>
2021-10-05 12:16:39 +0600

    mentors added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,15 +2,35 @@ import React from 'react';
 import Contact from '../Contact/Contact';
 import Courses from '../Courses/Courses';
 import LandPage from '../LandPage/LandPage';
-
+import SingleMentor from '../SingleMentor/SingleMentor';
+import useCourse from '../hooks/useCourse';
+import { Link } from 'react-router-dom';
 const Home = () => {
+    const [courses] = useCourse([])
+    let mentors = courses.slice(0, 4)
     return (
         <div >
             <LandPage></LandPage>
             <br />
             <Courses></Courses>
             <br />
+            <div className="container">
+                <div className="d-flex justify-content-between">
+                    <h3>Course Mentor</h3>
+                    <h3 className="my-auto"> <Link to='/about'>All Mentor</Link></h3>
+                </div>
+                <div className="row">
+                    {
+                        mentors.map(course => <SingleMentor key={course.id} course={course}></SingleMentor>)
+                    }
+
+                </div>
+            </div>
+            <br />
             <Contact></Contact>
+            <br />
+
+
         </div>
     );
 };
